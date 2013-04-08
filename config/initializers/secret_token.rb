b5ba04d699a298d04241f01cbd3e25df5d26e0c7
victor <victor@ubuntu.(none)>
2013-04-08 13:06:11 +0100

    Inicializando p5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P5::Application.config.secret_token = '3e6511bc756ae2a574aef3e35debed5d444bef4e4a647fc151e47e70a03c5618e5b4351fc9c90abaaed9749db6e0c8f199de77d89c9ebae14e1d51e64213051e'
